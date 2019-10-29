a97e9ef3ce016ed2350b41235f41faf36c4c4855
Andrew Belousoff <formatCvt@gmail.com>
2019-10-29 11:47:52 +0300

    Updates

---
+++ precache-manifest.14beb309aaa5bd714faf902d2e65d468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac444a776f5a91ab7f64a5772c3b73d",
+    "url": "/vk-apps-socialist/index.html"
+  },
+  {
+    "revision": "36f786d014f34135fc5e",
+    "url": "/vk-apps-socialist/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "a9ab0310757777ed9c04",
+    "url": "/vk-apps-socialist/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "36f786d014f34135fc5e",
+    "url": "/vk-apps-socialist/static/js/2.ff8ae4f9.chunk.js"
+  },
+  {
+    "revision": "a9ab0310757777ed9c04",
+    "url": "/vk-apps-socialist/static/js/main.c3334815.chunk.js"
+  },
+  {
+    "revision": "828be8f0aacfba579d90",
+    "url": "/vk-apps-socialist/static/js/runtime-main.b9bec409.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-socialist/static/media/persik.4e1ec840.png"
+  }
+]);
